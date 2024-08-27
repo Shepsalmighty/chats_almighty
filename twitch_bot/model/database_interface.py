ffa7208eeef184789dad62cbad191d5944d5928f
sheps <sheps@sheps.com>
2024-08-27 20:43:42 +0100

    move sqlalchemy db engine to another file/class

--- twitch_bot/model/database_interface.py
+++ twitch_bot/model/database_interface.py
@@ -1,9 +1,12 @@
+from sqlalchemy import create_engine
 
+#TODO: use the engine (created in __init___) to implement the class methods below
+#TODO look at sqlalchemy sessions/session objects
 
 class DataBase:
 
-    def __init__(self):
-        pass
+    def __init__(self, engine):
+        self.engine = create_engine("sqlite:///twitch_bot.db", echo=True)
 
     def add_link(self, channel, command_name, link):
         pass
