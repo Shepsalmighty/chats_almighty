9f846190c91e529bf6ecf3dc60ed37114e49f31b
sheps <sheps@sheps.com>
2024-10-07 20:40:42 +0100

    add to getmsg function

--- twitch_bot/view/twitch_chat_view.py
+++ twitch_bot/view/twitch_chat_view.py
@@ -14,16 +14,14 @@ from pathlib import Path
 import sqlite3
 from contextlib import closing
 import requests
+import time
 
 
+# TODO: Add a reply if anyone types "sudo !!" of "nice try"
 
-#TODO: Add a reply if anyone types "sudo !!" of "nice try"
-
-#INFO in the view class all user interactions are defined, it represents the user interface, commands like !discord or !help are part of it
+# INFO in the view class all user interactions are defined, it represents the user interface, commands like !discord or !help are part of it
 class TwitchChatView:
-
     USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
-
 
     def __init__(self, client_id, client_secret, path, target_channel):
         self.client_id = client_id
@@ -35,7 +33,9 @@ class TwitchChatView:
         self.lock = asyncio.Lock()
         # self.con = sqlite3.connect("twitch_bot.db")
         # self.cur = self.con.cursor()
-
+        self.users_notified = set()
+        # notified_lock = new lock for interacting with self.users_notified dict
+        self.notified_lock = asyncio.Lock()
 
     async def on_message(self, msg: ChatMessage):
         # print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
@@ -51,19 +51,19 @@ class TwitchChatView:
                                          (args[0].lstrip("!").lower(), self.channel)).fetchone()
 
             if args[0] == "!set" or args[0] == '!leavemsg':
-               pass
+                pass
             elif command_exists:
                 await self.link_command(msg)
             elif command_exists is None and msg.user.name == self.channel:
                 await msg.reply(f"add a link to your {msg.text} using \"!set !{msg.text.lstrip("!")} link\"   ")
+
             elif command_exists is None and msg.user.name != self.channel:
                 await msg.reply(f"no {msg.text} link yet")
 
-
             con.close()
-        else: print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
+        else:
+            print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
         await self.get_msg(msg)
-
 
     async def on_ready(self, ready_event: EventData):
         print('Bot is ready for work, joining channels')
@@ -72,23 +72,23 @@ class TwitchChatView:
         await ready_event.chat.join_room(self.channel)
         # you can do other bot initialization things in here
 
-    #TODO: add "!stupid" and "!smrt" incremental counters for chat
-    #TODO" add "!hug" command
-    #TODO add !lurk
+    # TODO: add "!stupid" and "!smrt" incremental counters for chat
+    # TODO" add "!hug" command
+    # TODO add !lurk
 
-    #TODO: add !help
-    #!set(!example) over writes existing command with channel owners input links etc
+    # TODO: add !help
+    # !set(!example) over writes existing command with channel owners input links etc
     async def set_command(self, cmd: ChatCommand):
         if cmd.user.name == self.channel:
-            args = cmd.parameter.split(" ",1)
+            args = cmd.parameter.split(" ", 1)
             if len(args) < 2:
                 await cmd.reply("command set incorrectly, did you forget to add the link?")
 
-            #creating a lock to avoid multiple cursor objects accessing the table at once causing issues
+            # creating a lock to avoid multiple cursor objects accessing the table at once causing issues
             # await self.lock.acquire()
             async with self.lock:
 
-                #creating connection to db and cursor objects
+                # creating connection to db and cursor objects
                 with closing(sqlite3.connect("twitch_bot.db")) as con:
                     cur = con.cursor()
                     try:
@@ -112,20 +112,19 @@ class TwitchChatView:
 
 
         else:
-           await cmd.reply("you cannot change bot settings")
+            await cmd.reply("you cannot change bot settings")
 
     async def link_command(self, msg: ChatMessage):
         # if cmd.user.name == self.channel:
-            # self.allowed_user_values.append(cmd.parameter[1])
+        # self.allowed_user_values.append(cmd.parameter[1])
 
         if msg.text.startswith("!"):
             args = msg.text.split(" ", 1)
 
-
-            #if (cmd.text.lstrip("!") == command_exists[0]):
+            # if (cmd.text.lstrip("!") == command_exists[0]):
             async with self.lock:
                 with closing(sqlite3.connect("twitch_bot.db")) as con:
-                # con = sqlite3.connect("twitch_bot.db")
+                    # con = sqlite3.connect("twitch_bot.db")
                     cur = con.cursor()
                     try:
                         link_exists = cur.execute('''SELECT l.linktext FROM links l
@@ -143,17 +142,14 @@ class TwitchChatView:
                     except sqlite3.Error as e:
                         print(f'An error occurred: {e}')
 
-
-
     async def leavemsg(self, msg: ChatMessage):
 
         args = msg.text.split(" ", 2)
         if not len(args) == 3:
             return await msg.reply("it looks like you used this command incorrectly, did you forget your message?")
 
-
         username, message = args[1], args[2]
-        #TODO - implement regex instead of below
+        # TODO - implement regex instead of below
         if len(args) == 3 and msg.text.startswith("!") and args[0] == "!leavemsg" and args[1].startswith("@"):
             print("we got here")
             async with self.lock:
@@ -167,8 +163,9 @@ class TwitchChatView:
                                        (msg.user.name,)).fetchone()[0] < 10:
 
                             print(f'{msg.user.name}, {args[1][1:]}, {args[2]}')
-                            cur.execute('INSERT INTO messages (`sender_id`, `receiver_id`, `messagetext`) VALUES (?,?,?)',
-                                        (msg.user.name, args[1][1:], args[2]))
+                            cur.execute(
+                                'INSERT INTO messages (`sender_id`, `receiver_id`, `messagetext`) VALUES (?,?,?)',
+                                (msg.user.name, args[1][1:], args[2]))
                             con.commit()
                         else:
                             await msg.reply("""3 msg limit reached, msgs are deleted after each stream ends or
@@ -176,7 +173,7 @@ class TwitchChatView:
                     except sqlite3.Error as e:
                         print(f'An error occurred: {e}')
 
-    #INFO - CHECK USER EXISTS - https://dev.twitch.tv/docs/api/reference/#get-users
+    # INFO - CHECK USER EXISTS - https://dev.twitch.tv/docs/api/reference/#get-users
 
     async def get_msg(self, msg: ChatMessage):
 
@@ -185,59 +182,43 @@ class TwitchChatView:
                 cur = con.cursor()
                 # message_count = cur.execute('SELECT COUNT(*) FROM messages WHERE receiver_id = ?', (msg.user.name,))
                 target = cur.execute('SELECT receiver_id FROM messages')
-                users_with_msgs = tuple()
+                users_with_msgs = set()
                 for name in target:
-                    users_with_msgs += name
+                    users_with_msgs.add(name[0])
 
-
-                sender_id = cur.execute('SELECT sender_id FROM messages WHERE receiver_id = ?', (msg.user.name,))
                 sender_names = []
+                sender_id = cur.execute('SELECT sender_id FROM messages WHERE receiver_id = ?', (msg.user.name,))
+                # after this section you dont need self.lock anymore (the db stuff ends here)
                 for name in sender_id:
-                    sender_names.append(name)
+                    sender_names.append(name[0])
+        # sender_names = []
+        # for name in sender_id:
+        #     sender_names.append(name)
+        print(users_with_msgs, self.users_notified)
+        if msg.user.name in users_with_msgs and msg.user.name not in self.users_notified:
+            await msg.reply(
+                f'you have {len(sender_names)} messages stored from {sender_names}, to get a message use !getmsg @username')
+            async with self.notified_lock:
+                self.users_notified.add(msg.user.name)
 
-                try:
-                    # if msg.user.name.text in stored_msgs:
-                    #     print("get msg working")
-                    # messages = cur.execute('SELECT messagetext, sender_id, uid FROM messages WHERE receiver_id = ?',
-                    #                        (msg.user.name,))
-                    if msg.user.name in users_with_msgs:
-                        await msg.reply(f'you have {len(sender_names)} messages stored from {sender_names}, to get a message use !getmsg @username')
+            args = msg.text.split(" ", 1)
+            if len(args) > 1 and args[1].lstrip("@") not in sender_names:
+                await msg.reply(
+                    f'To get a message use !getmsg @username. You have {len(sender_names)} messages stored from: {sender_names}')
+            else:
+                print("hello world")
 
-                    # for row in messages:
+            # messages = cur.execute('SELECT messagetext, sender_id, uid FROM messages WHERE receiver_id = ?',
+            #                        (msg.user.name,))
 
-                        # await msg.reply(f'{row[1]} left you a message: {row[0]}')
-                        # cur.execute('DELETE FROM messages WHERE uid = ?', (row[2],))
-                        # con.commit()
-                    # else: pass
-                except sqlite3.Error as e:
-                        print(f'An error occurred: {e}')
+            # for row in messages:
 
+            # await msg.reply(f'{row[1]} left you a message: {row[0]}')
+            # cur.execute('DELETE FROM messages WHERE uid = ?', (row[2],))
+            # con.commit()
+            # else: pass
 
-
-
-
-
-    # async def discord_command(self, cmd: ChatCommand):
-    #     # TODO: TARGET_CHANNEL won't work when the bot has it's own acc as that acc will never be setting params in other peoples chats
-    #     print(cmd.name)
-    #     if "discord" in self.user_values:
-    #         await cmd.reply(self.user_values.get("discord"))
-    #     elif len(cmd.parameter) == 0 and cmd.user.name == self.channel:
-    #         await cmd.reply("add a link to your discord using \"!set !discord link\"   ")
-    #     elif len(cmd.parameter) >= 0 and cmd.user.name != self.channel:
-    #         await cmd.reply("no discord link yet")
-    #
-    # async def youtube_command(self, cmd: ChatCommand):
-    #     # TODO: TARGET_CHANNEL won't work when the bot has it's own acc as that acc will never be setting params in other peoples chats
-    #     if "youtube" in self.user_values:
-    #         await cmd.reply(self.user_values.get("youtube"))
-    #     elif len(cmd.parameter) == 0 and cmd.user.name == self.channel:
-    #         await cmd.reply("add a link to your youtube using \"!set !discord - paste link here:\"   ")
-    #     elif len(cmd.parameter) >= 0 and cmd.user.name != self.channel:
-    #         await cmd.reply("no youtube link yet")
-
-
-#TODO: add some tests
+    # TODO: add some tests
 
     # this is where we set up the bot
     async def run(self):
@@ -267,10 +248,7 @@ class TwitchChatView:
         chat.register_command('set', self.set_command)
         chat.register_command('leavemsg', self.leavemsg)
         chat.register_command('getmsg', self.get_msg)
-#TODO make !help command
-
-
-
+        # TODO make !help command
 
         # we are done with our setup, lets start this bot up!
         chat.start()
